490afaaf7c479fb826413457387654fa00875422
Nelson Vargas <51175677+neovave@users.noreply.github.com>
2019-12-20 16:52:59 -0400

    Add files via upload

---
+++ 2019_12_20_032516_create_permiso_rols_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermisoRolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso_rols', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('rol_id');
+            $table->unsignedBigInteger('permiso_id');
+            $table->timestamps();
+            $table->foreign('rol_id')->references('id')->on('rols')->onDelete('cascade');
+            $table->foreign('permiso_id')->references('id')->on('permisos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso_rols');
+    }
+}
---
+++ 2019_12_20_034751_create_usuario_rols_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioRolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario_rols', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('rol_id');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('rol_id')->references('id')->on('rols')->onDelete('cascade');
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario_rols');
+    }
+}
